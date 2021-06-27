81d1c0b716624df5d07b6e1073d98d064647c670
Jamis <hoojamis@gmail.com>
2021-06-27 18:41:46 +0800

    partial download

--- webdav_get.py
+++ webdav_get.py
@@ -1,5 +1,6 @@
 from app import app, config
-from flask import Response, stream_with_context, abort
+import parse
+from flask import request, Response, stream_with_context, abort
 import azure.storage.filedatalake
 
 
@@ -7,8 +8,28 @@ import azure.storage.filedatalake
 def get(path):
     file_client = azure.storage.filedatalake.DataLakeFileClient(
         "https://{}.dfs.core.windows.net".format(config.storage_account_name), config.storage_container, path, credential=config.storage_account_key)
+
+    file_offset = None
+    file_length = None
+    if request.headers.get("Range"):
+        nums = parse.parse("bytes={:d}-{:d}", request.headers.get("Range"))
+        if not nums:
+            nums = parse.parse("bytes={:d}-", request.headers.get("Range"))
+        if not nums or len(nums.fixed) < 1 or len(nums.fixed) > 2:
+            abort(400)
+        file_offset = nums[0]
+        if len(nums.fixed) == 2:
+            file_length = nums[1] - file_offset + 1
+
+    raw_response = None
+
+    def callback(response):
+        nonlocal raw_response
+        raw_response = response.http_response
+
     try:
-        download_response = file_client.download_file()
+        download_response = file_client.download_file(
+            offset=file_offset, length=file_length, raw_response_hook=callback)
     except azure.core.exceptions.ResourceNotFoundError:
         abort(404)
 
@@ -19,7 +40,8 @@ def get(path):
     if is_directory:
         raise NotImplementedError
 
-    response = Response(stream_with_context(download_response.chunks()))
+    response = Response(stream_with_context(
+        download_response.chunks()), status=206 if file_offset else 200)
     response.accept_ranges = "bytes"
     response.set_etag(properties.etag.strip("\""))
     if content_settings["cache_control"]:
@@ -28,10 +50,14 @@ def get(path):
         response.content_encoding = content_settings["content_encoding"]
     if content_settings["content_language"]:
         response.content_language = content_settings["content_language"]
-    response.content_length = download_response.size
     if content_settings["content_md5"]:
         response.content_md5 = content_settings["content_md5"]
     if content_settings["content_type"]:
         response.content_type = content_settings["content_type"]
+    if file_offset:
+        nums = parse.parse("bytes {:d}-{:d}/{:d}",
+                           raw_response.headers["Content-Range"])
+        response.content_range.set(nums[0], nums[1] + 1, nums[2])
+    response.content_length = int(raw_response.headers["Content-Length"])
 
     return response
